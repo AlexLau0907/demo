69c86a20729841641c8b575703753f50eeae68de
AlexLau0907 <yuzhiliu0907@126.com>
2016-10-28 12:51:19 +0800

    Redux UI Navigation demo

---
+++ app/components/Samples.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image
+} from 'react-native'
+
+
+const Samples = () => {
+  <View style={styles.container}>
+    <Text style={styles.Text}>
+      Hello from Sample!
+    </Text>
+  </View>
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff53f5',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    marginTop: 100,
+    fontSize: 24,
+    textAlign: 'center'
+  },
+  image: {
+    width: 250,
+    height: 250
+  }
+})
+
+export default Samples
---
+++ index.ios.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React from 'react'
+import { AppRegistry } from 'react-native'
+
+import configureStore from './app/store/configureStore'
+const store = configureStore()
+
+import TabsRootContainer from './app/containers/tabsRootContainer'
+import { Provider } from 'react-redux'
+
+const App = () => (
+  <Provider store={store}>
+    <TabsRootContainer />
+  </Provider>
+)
+AppRegistry.registerComponent('demo', () => App)
